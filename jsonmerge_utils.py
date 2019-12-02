491df379c279265425d3739a6ee56fbe404e453b
Nikhil Prakash Patil <nikhilpatil.patil0@gmail.com>
2019-12-03 01:08:48 +0530

    reduced imports and improved the program flow

--- jsonmerge_utils.py
+++ jsonmerge_utils.py
@@ -4,10 +4,8 @@ Created on Fri Nov 15 04:21:26 2019
 
 @author: GRENTOR
 """
-import json
 import os
 #from tqdm import tqdm
-from merge_files import merge_object
 from merge_files import Merge
 
 
@@ -23,39 +21,48 @@ def output_folder(data_dir):
     os.mkdir(path)
     return path
 
-def file_list(data_dir, input_prefix):
+def path_creator(data_dir, prefix):
     """
-    Lists the json files with the given input prefix
+    Creates a path with prefix at end
     """
-    i = 1
-    path = os.path.join(data_dir, '{}{}{}'.format(input_prefix, i, '.json'))
-    while os.path.isfile(path):
-        yield path
-        i += 1
-        path = os.path.join(data_dir, '{}{}{}'.format(input_prefix, i, '.json'))
+    return os.path.join(data_dir, prefix)
+
+class Error(Exception):
+    """Base class for other exceptions"""
+
+class FolderNotFoundError(Error):
+    """Raised when the Folder path is not found"""
 
 def main():
     """
     Driver Function
     """
-    data_dir = input('Folder Path: ')
-    input_prefix = input('I/P Prefix: ')
-    output_prefix = input('O/P Prefix: ')
-    max_file_size = int(input('Max File Size: '))
-    end = False
-    merger, root_keys = merge_object(os.path.join(data_dir, '{}{}'.format(input_prefix,'1.json')))
-    merge = Merge(data_dir, output_prefix, max_file_size, merger, root_keys)
-    count = 0
-    for i in file_list(data_dir, input_prefix):
-        print(i)
-        with open(i) as input_file:
-            obj = json.load(input_file)
-            merge.merge_file(obj, end)
-        count+=1
-    end = True
-    merge.merge_file(None, end)
-    print('{} Files Processed'.format(count))
-    print('Done!!')
+    try:
+        data_dir = input('Folder Path: ')
+        if not os.path.exists(data_dir):
+            raise FolderNotFoundError
+        input_prefix = input('I/P Prefix: ')
+        if not os.path.isfile('{}{}'.format(path_creator(data_dir, input_prefix), '1.json')):
+            raise FileNotFoundError
+        output_prefix = input('O/P Prefix: ')
+        max_file_size = int(input('Max File Size: '))
+
+        merge = Merge(path_creator(data_dir, input_prefix),
+                      path_creator(data_dir, output_prefix),
+                      max_file_size)
+
+        """ merge = Merge(path_creator(data_dir, input_prefix),
+                      path_creator(output_folder(data_dir), output_prefix),
+                      max_file_size) """
+
+        merge.merge()
+
+    except FileNotFoundError:
+        print('Please check the input prefix !!')
+    except FolderNotFoundError:
+        print('No such directory exists !!')
+    except ValueError:
+        print('File size larger than given Max File Size !!')
 
 if __name__ == '__main__':
     main()
--- merge_files.py
+++ merge_files.py
@@ -9,12 +9,14 @@ import os
 from jsonmerge import Merger
 from schema_modified import schema_builder
 
-def merge_object(data_dir):
+def merge_object(data_dir, max_file_size):
     """"
     Generates and returns a merge object for merging files
     """
     with open(data_dir) as input_file:
         obj = json.load(input_file)
+        if sizeof(obj) > max_file_size:
+            raise ValueError
         return Merger(schema_builder(obj)), list(obj.keys())
 
 def sizeof(obj):
@@ -29,14 +31,13 @@ class Merge:
     """
     Merge class for merging json files
     """
-    def __init__(self, output_path, output_prefix, max_file_size, merger, root_keys):
+    def __init__(self, input_path, output_path, max_file_size):
         self.base = None
         self.output_path = output_path
-        self.output_prefix = output_prefix
+        self.input_path = input_path
         self.max_file_size = max_file_size
-        self.root_keys = root_keys
-        self.merger = merger
         self.counter = 1
+        self.merger, self.root_keys = merge_object('{}{}'.format(self.input_path, '1.json'), self.max_file_size)
 
     def redundant_obj(self):
         """
@@ -46,24 +47,56 @@ class Merge:
             set_of_jsons = {json.dumps(d, sort_keys=False) for d in self.base[i]}
             self.base[i] = [json.loads(t) for t in set_of_jsons]
 
+    def file_list(self):
+        """
+        Lists the json files with the given input prefix
+        """
+        i = 1
+        path = '{}{}{}'.format(self.input_path, i, '.json')
+        while os.path.isfile(path):
+            yield path
+            i += 1
+            path = '{}{}{}'.format(self.input_path, i, '.json')
+
+    def merge(self):
+        """
+        merging driver function
+        """
+        end = False
+        count = 0
+        for i in self.file_list():
+            print(i)
+            with open(i) as input_file:
+                obj = json.load(input_file)
+                self.merge_file(obj, end)
+            count += 1
+        end = True
+        self.merge_file(None, end)
+        print('Done!!')
+        print('{} Files Processed'.format(count))
+        print('{} Result files created'.format(self.counter))
+
     def merge_file(self, head, end):
         """
         Merges the files and writes to json file upon reaching limit
         """
         base_size = sizeof(self.base)
+        head_size = sizeof(head)
+        if head_size > self.max_file_size:
+            raise ValueError
         if not end:
-            if (base_size < self.max_file_size) and ((base_size+sizeof(head)) <= self.max_file_size):
+            if (base_size < self.max_file_size) and ((base_size+head_size) <= self.max_file_size):
                 self.base = (self.merger).merge(self.base, head)
                 #self.redundant_obj()
             else:
-                path = os.path.join(self.output_path, '{}{}{}'.format(self.output_prefix, self.counter, '.json'))
-                with open(path, 'w', encoding='utf-8') as op:
-                    json.dump(self.base, op, ensure_ascii=False)
+                path = '{}{}{}'.format(self.output_path, self.counter, '.json')
+                with open(path, 'w', encoding='utf-8') as output_file:
+                    json.dump(self.base, output_file, ensure_ascii=False)
                 self.counter += 1
                 self.base = None
                 self.base = (self.merger).merge(self.base, head)
         else:
             if self.base:
-                path = os.path.join(self.output_path, '{}{}{}'.format(self.output_prefix, self.counter, '.json'))
-                with open(path, 'w', encoding='utf-8') as op:
-                    json.dump(self.base, op, ensure_ascii=False)
+                path = '{}{}{}'.format(self.output_path, self.counter, '.json')
+                with open(path, 'w', encoding='utf-8') as output_file:
+                    json.dump(self.base, output_file, ensure_ascii=False)
